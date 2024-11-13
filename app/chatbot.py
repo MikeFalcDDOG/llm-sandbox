673ffad7bf561ed57277d40c227d3146e8fae963
MikeFalcDDOG <michael.falcone@datadoghq.com>
2024-11-13 14:36:18 -0700

    supplementing the chatbot with the websites to crawl for dining recommendations:

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,57 +1,85 @@
-import os
-
-#!/usr/bin/env python
-from typing import List, Union
-
-from fastapi import FastAPI
-from langchain_core.chat_history import InMemoryChatMessageHistory
-from langchain_core.output_parsers import StrOutputParser
+import bs4
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI
-from pydantic import BaseModel, Field
-from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, SystemMessage
-from langserve import add_routes
-app = FastAPI()
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-llmopenai = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-# Declare a chain
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", "You are a helpful AI assistant. Please provide a concise list of course with university that suits users question."),
-#         MessagesPlaceholder(variable_name="history"),
-#         ("human", "{human_input}"),
-#     ]
-# )
 
-prompt = ChatPromptTemplate.from_messages(
+### Construct retriever ###
+loader = WebBaseLoader(
+    ["https://secretdenver.com/foodie-bucket-list-denver/",
+     "https://www.denver.org/food-drink/restaurants/",
+     "https://denver.eater.com/maps/best-restaurants-denver-eater-38"])
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+retriever = vectorstore.as_retriever()
+
+
+### Contextualize question ###
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful AI assistant that recommends courses along with universities to the students. Please provide a concise list of course with university that suits users question."),
-        MessagesPlaceholder(variable_name="messages"),
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
     ]
 )
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
 
-chain = prompt | llmopenai | StrOutputParser()
 
-def get_session_history():
-    return InMemoryChatMessageHistory()
+### Answer question ###
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
 
-class InputChat(BaseModel):
-    """Input for the chat endpoint."""
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
 
-    messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
-        ...,
-        description="The chat messages representing the current conversation.",
-    )
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
 
-chain_with_history = RunnableWithMessageHistory(
-    chain,
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
     get_session_history,
     input_messages_key="input",
-    history_messages_key="chat_history"
-).with_types(input_type=InputChat)
-
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
 add_routes(
     app,
     chain_with_history,
